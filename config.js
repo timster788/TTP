b69d56edb7b5e3ebc3099869a0301abafbf095d2
Timothy Dao <timster78811@gmail.com>
2019-08-16 23:34:04 -0400

    added changes to db

--- config.js
+++ config.js
@@ -3,12 +3,11 @@ require('dotenv').config();
 module.exports = {
   PORT: process.env.PORT || 8080,
   CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'http://localhost:3000',
-  MONGODB_URI: process.env.MONGODB_URI || 'mongodb://localhost/anime_tracker',
+  MONGODB_URI:
+    process.env.MONGODB_URI ||
+    'mongodb://heroku_qwsh37sh:5rd370ghjogfcso7jbd8ukiq8j@ds263917.mlab.com:63917/heroku_qwsh37sh',
   TEST_DATABASE_URL:
-    process.env.TEST_DATABASE_URL || 'mongodb://localhost/anime_tracker',
+    process.env.TEST_DATABASE_URL || 'mongodb://localhost/test',
   JWT_SECRET: process.env.JWT_SECRET,
-  JWT_EXPIRY: process.env.JWT_EXPIRY || 15 * 60 * 1000,
-  MONGODB_URI: process.env.MONGODB_URI || 'mongodb://localhost/anime_tracker',
-  TEST_MONGODB_URI:
-    process.env.TEST_MONGODB_URI || 'mongodb://localhost/anime_tracker-test'
+  JWT_EXPIRY: process.env.JWT_EXPIRY || 15 * 60 * 1000
 };
