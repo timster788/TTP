d623dc652ac35aa223fd0d74c5e91bb66e6a0b05
Timothy Dao <timster78811@gmail.com>
2019-08-24 03:23:29 -0400

    edit

--- config.js
+++ config.js
@@ -6,8 +6,8 @@ module.exports = {
   MONGODB_URI:
     process.env.MONGODB_URI ||
     'mongodb://heroku_qwsh37sh:5rd370ghjogfcso7jbd8ukiq8j@ds263917.mlab.com:63917/heroku_qwsh37sh',
-  TEST_DATABASE_URL:
-    process.env.TEST_DATABASE_URL || 'mongodb://localhost/test',
+  // TEST_DATABASE_URL:
+  //   process.env.TEST_DATABASE_URL || 'mongodb://localhost/test',
   JWT_SECRET: process.env.JWT_SECRET,
   JWT_EXPIRY: process.env.JWT_EXPIRY || 15 * 60 * 1000
 };
